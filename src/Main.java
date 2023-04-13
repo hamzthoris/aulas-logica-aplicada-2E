61af85d51a621c0dc9a1aeec0bc448448b7a0078
rm86704 <sliceaddam@gmail.com>
2023-04-13 11:00:23 -0300

    Criados laços enquanto, para e faça

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int contador = 0;
+
+        while (contador < 10) {
+            System.out.println("Contador = " + contador);
+            //incremento
+            contador++;
+            // Laço do para faça - for
+            for (int x = 0; x < 10; x++) {
+                System.out.print("Contador = " + x);
+            }
+            int cont = 1;
+            do {
+                //Processo
+                System.out.println(cont);
+                cont++;
+            } while (cont <= 10);
+        }
     }
 }
