fd5c9491856edad4345721b14880f8427b73ddfc
rm86704 <sliceaddam@gmail.com>
2023-05-04 11:55:20 -0300

    Aula de  introdução 00

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int contador = 0;
+        //Aula introdutória de Orientação à Objetos
 
-        while (contador < 10) {
-            System.out.println("Contador = " + contador);
-            //incremento
-            contador++;
-            // Laço do para faça - for
-            for (int x = 0; x < 10; x++) {
-                System.out.print("Contador = " + x);
-            }
-            int cont = 1;
-            do {
-                //Processo
-                System.out.println(cont);
-                cont++;
-            } while (cont <= 10);
-        }
     }
 }
