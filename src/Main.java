b741416d7086b57084afde0a62d8f953994457ca
rm86704 <sliceaddam@gmail.com>
2023-05-18 12:26:34 -0300

    É uma forma nova de programar, criar uma classe pessoa e utiliza-la como tipo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         //Aula introdutória de Orientação à Objetos
 
+        Pessoa adao = new Pessoa();
+        Pessoa qualquer;
+        //Instanciação de Objeto
+        qualquer = new Pessoa();
+        // Definição do formato da pessoa qualquer
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+
+    //Atributos da Pessoa
+    public String nome;
+    public String sobrenome;
+    // Métodos da Classe pessoa
+    public void falar() {
+        System.out.println("Falei");
+    }
+}
